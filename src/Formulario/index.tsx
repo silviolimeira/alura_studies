f6b282eb50ec81d807bc29b5b854b30a921ee00f
Silvio Limeira <sicemal@gmail.com>
2023-04-24 12:45:10 -0300

    initial version

---
+++ src/Formulario/index.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Botao from '../components/Botao';
+
+class Formulario extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label htmlFor="tarefa"></label>
+                    <input type="text" name="tarefa" id="tarefa" placeholder='O que você quer estudar' required></input>
+                </div>
+                <div>
+                    <label>Tempo</label>
+                    <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required></input>
+                </div>
+                <Botao />
+            </form>
+        )
+    }
+}
+
+export default Formulario;
