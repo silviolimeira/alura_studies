8e8b59d0b3ea7cab11b914b065157fc97ad152bc
silviolimeira <sicemal@gmail.com>
2023-04-25 13:21:40 -0300

    Use state introduction

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -17,7 +17,7 @@ class Formulario extends React.Component {
                     <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required></input>
                 </div>
                 <Botao>Adicionar 1</Botao>
-                <p>456</p>
+                <p>001</p>
             </form >
         )
     }
